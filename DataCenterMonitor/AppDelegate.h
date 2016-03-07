c7207cb63a0397b2a1f301f53270ee39ab9c1f44
epoluodi <12163687@qq.com>
2016-03-07 10:55:49 +0800

    创建

---
+++ DataCenterMonitor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DataCenterMonitor
+//
+//  Created by Stereo on 16/3/7.
+//  Copyright © 2016年 pxzdh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DataCenterMonitor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DataCenterMonitor
+//
+//  Created by Stereo on 16/3/7.
+//  Copyright © 2016年 pxzdh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
